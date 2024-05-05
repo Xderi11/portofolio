67e2783784b1fc428a178dbf3d630df49f7bea54
Xderi11 <derinanda27@gmail.com>
2024-05-05 21:14:56 +0700

    upload

---
+++ src/assets/Routes/AllCertificate.js
@@ -0,0 +1,17 @@
+import Certificate1 from "../img/Certificate1.jpg";
+import Certificate2 from "../img/Certificate2.jpg";
+import Certificate3 from "../img/Certificate3.jpg";
+import Certificate4 from "../img/Certificate4.jpg";
+import Certificate5 from "../img/Certificate5.jpg";
+import Certificate6 from "../img/Certificate6.jpg";
+
+const certificateImages = [
+  Certificate1,
+  Certificate2,
+  Certificate3,
+  Certificate4,
+  Certificate5,
+  Certificate6,
+];
+
+export default certificateImages;
---
+++ src/assets/Routes/AllProject.js
@@ -0,0 +1,30 @@
+const allProjects = [
+  {
+    title: 'Point Of Sale',
+    img: require('../img/Project1.png'),
+    description:
+      'Our Point of Sale (POS) application is a modern solution for your business sales and inventory management. With an intuitive interface, features include inventory management, fast sales, sales tracking, customer management, reports and flexible integrations..',
+    websiteUrl: '',
+    techStack: ['Figma'],
+  },
+  {
+    title: 'population data collection website',
+    img: require('../img/Project2.png'),
+    description:
+      'population data collection website.This website was created for the Tanjung Diamond Barat Village Office.',
+ 
+    techStack: ['Next.js', 'Express', 'Firebase'],
+  },
+  {
+    title: 'Portofolio',
+    img: require('../img/Project3.png'),
+    description:
+      'This portfolio website is my first personal portfolio, this portfolio was created using the react.js language',
+    websiteUrl: '',
+    githubUrl: '',
+    techStack: ['Javascript', 'HTML5', 'REACT'],
+  },
+
+];
+
+export default allProjects;
